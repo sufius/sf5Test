44aeeff510ce319894cfdadd7cfd7973f2770b8b
imvlandau <itimvlandau@gmail.com>
2021-10-06 02:03:25 +0200

    activate basic http authentication support

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
